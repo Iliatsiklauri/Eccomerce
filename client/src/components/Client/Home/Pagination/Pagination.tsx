424fbce012c944f29ecce72559c2945d169532a6
Ilia Tsiklauri <ilia.tsiklauri@dataart.com>
2024-09-25 20:09:51 +0400

    fix:pagination button logic

--- client/src/components/Client/Home/Pagination/Pagination.tsx
+++ client/src/components/Client/Home/Pagination/Pagination.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 type PropType = {
@@ -7,26 +8,49 @@ type PropType = {
 };
 
 export default function Pagination({ total, active }: PropType) {
-  const pageCount = total ? Math.ceil(total / 20) : 0;
-  const pageButtons = [];
+  const params = useSearchParams();
+  const page = params.get("page");
+  const TotalPageCount = total ? Math.ceil(total / 20) : 0;
+  const indicator = page ? Number(page) : 1;
 
-  for (let i = 1; i <= pageCount; i++) {
+  const pageButtons = [];
+  for (let i = 1; i <= TotalPageCount; i++) {
     pageButtons.push(i);
   }
 
   return (
     <div className="join">
-      <div className="join-item btn">«</div>
-      {pageButtons.map((page) => (
+      {indicator > 1 ? (
         <Link
-          key={page}
           className="join-item btn"
-          href={`/products?category=${active}&page=${page}`}
+          href={`/products?category=${active}&page=${indicator - 1}`}
         >
-          {page}
+          «
+        </Link>
+      ) : (
+        <div className="join-item btn disabled">«</div>
+      )}
+
+      {pageButtons.map((pg) => (
+        <Link
+          key={pg}
+          className={`join-item btn ${indicator === pg && "btn-active"}`}
+          href={`/products?category=${active}&page=${pg}`}
+        >
+          {pg}
         </Link>
       ))}
-      <div className="join-item btn">»</div>
+
+      {indicator < TotalPageCount ? (
+        <Link
+          className="join-item btn"
+          href={`/products?category=${active}&page=${indicator + 1}`}
+        >
+          »
+        </Link>
+      ) : (
+        <div className="join-item btn disabled">»</div>
+      )}
     </div>
   );
 }
