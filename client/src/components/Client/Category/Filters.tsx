8f48bed9b8882a89f90198e289452488e6ed530f
Ilia Tsiklauri <ilia.tsiklauri@dataart.com>
2024-10-10 15:48:34 +0400

    test test

--- client/src/components/Client/Category/Filters.tsx
+++ client/src/components/Client/Category/Filters.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Filters() {
-  return <div className="w-full flex items-center justify-start"></div>;
+  return (
+    <div className="w-full flex items-center justify-start">
+      filters filrers
+    </div>
+  );
 }
