73f5eaec2bb6715e97448db1b272f9ba584e6fc2
Ilia Tsiklauri <ilia.tsiklauri@dataart.com>
2024-10-11 15:55:05 +0400

    test1

--- client/src/components/Shared/Header/Header.tsx
+++ client/src/components/Shared/Header/Header.tsx
@@ -1,18 +1,19 @@
-"use client";
-import HeaderCartSection from "./HeaderCartSection";
-import SearchInput from "./SearchInput";
-import Link from "next/link";
+'use client';
+import HeaderCartSection from './HeaderCartSection';
+import SearchInput from './SearchInput';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="w-full h-[80px] bg-darkBrown flex items-center justify-center z-50 shadow-sm relative">
       <Link
         className="text-white text-3xl left-[5%] absolute font-semibold cursor-pointer"
-        href={"/"}
+        href={'/'}
       >
         Eccomerce
       </Link>
-      <Link href={"/admin/Products?mode=read"}>ADMIN PANEL</Link>
+      <h1>hesssllo</h1>
+      <Link href={'/admin/Products?mode=read'}>ADMIN PANEL</Link>
       <div className="container1 flex justify-between items-center">
         <SearchInput />
         <HeaderCartSection />
